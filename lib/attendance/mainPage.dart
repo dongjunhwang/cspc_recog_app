f76449740c28f4e21e84a6741728a2bacaefe2cd
ljy2855 <ljy2855@naver.com>
2021-08-26 20:55:25 +0900

    attendance UI fix

--- lib/attendance/mainPage.dart
+++ lib/attendance/mainPage.dart
@@ -1,6 +1,7 @@
 import 'package:cspc_recog/attendance/models/profile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'dart:math';
 
 import '../urls.dart';
@@ -28,10 +29,26 @@ class _AttendancePageState extends State<AttendancePage> {
     Colors.orange,
   ];
 
+  final List<Icon> rankingIconList = [
+    Icon(
+      Foundation.crown,
+      color: Colors.amber[400],
+    ),
+    Icon(
+      Foundation.crown,
+      color: Colors.grey[400],
+    ),
+    Icon(
+      Foundation.crown,
+      color: Colors.brown[400],
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
+        // if up swipe, reload
         onVerticalDragUpdate: (dragUpdateDetails) {
           setState(() {});
         },
@@ -60,7 +77,7 @@ class _AttendancePageState extends State<AttendancePage> {
     );
   }
 
-  //TODO 현재 온라인인 유저를 표시
+  //현재 온라인 count
   Widget profileCount(final List<ProfileModel> profileList) {
     int cnt = 0;
     profileList.forEach((element) {
@@ -81,6 +98,7 @@ class _AttendancePageState extends State<AttendancePage> {
   String formatDuration(Duration d) =>
       d.toString().split('.').first.padLeft(8, "0");
 
+  // 누적 방문시간 랭킹 표시
   Widget visitTimeRanking(List<ProfileModel> profileList) {
     profileList.sort(
       (a, b) => b.visitTimeSum.compareTo(a.visitTimeSum),
@@ -111,9 +129,8 @@ class _AttendancePageState extends State<AttendancePage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("${i + 1}등 ${profileList[i].nickName} "),
-                          Text(
-                              "${formatDuration(profileList[i].visitTimeSum)}"),
+                          rankingIconList[i],
+                          Text("${profileList[i].nickName} "),
                         ],
                       )
                   ],
@@ -124,92 +141,90 @@ class _AttendancePageState extends State<AttendancePage> {
     );
   }
 
+  //프로필 리스트 출력
   Widget profileView(List<ProfileModel> profileList) {
     // online profile sort
     profileList.sort((a, b) => b.isOnline ? 1 : -1);
     return Container(
       alignment: Alignment.topLeft,
       padding: EdgeInsets.all(10),
-      child: GridView.count(
-        crossAxisCount: 3,
-        children: [
-          for (ProfileModel profile in profileList)
-            Container(
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Stack(
-                        children: [
-                          profile.profileImageUrl == null
-                              ? Icon(
-                                  Icons.account_circle,
-                                  size: 50,
-                                  color: profileColorList[Random()
-                                      .nextInt(profileColorList.length)],
-                                )
-                              : Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                      fit: BoxFit.fill,
-                                      image: NetworkImage(UrlPrefix.urls
-                                              .substring(0,
-                                                  UrlPrefix.urls.length - 1) +
-                                          profile.profileImageUrl),
-                                    ),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: GridView.count(
+          crossAxisCount: 4,
+          children: [
+            for (ProfileModel profile in profileList)
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        profile.profileImageUrl == null
+                            ? Icon(
+                                Icons.account_circle,
+                                size: 50,
+                                color: profileColorList[
+                                    Random().nextInt(profileColorList.length)],
+                              )
+                            : Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(UrlPrefix.urls
+                                            .substring(
+                                                0, UrlPrefix.urls.length - 1) +
+                                        profile.profileImageUrl),
                                   ),
                                 ),
-                          Positioned(
-                            right: 3,
-                            bottom: 3,
-                            child: Container(
-                              width: 20,
-                              height: 20,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.white,
                               ),
+                        Positioned(
+                          right: 3,
+                          bottom: 3,
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
                             ),
                           ),
-                          Positioned(
-                            right: 5,
-                            bottom: 5,
-                            child: Container(
-                              width: 15,
-                              height: 15,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: profile.isOnline
-                                    ? Colors.green
-                                    : Colors.grey,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        profile.nickName,
-                        style: TextStyle(
-                          fontSize: 20,
                         ),
+                        Positioned(
+                          right: 5,
+                          bottom: 5,
+                          child: Container(
+                            width: 15,
+                            height: 15,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color:
+                                  profile.isOnline ? Colors.green : Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      profile.nickName,
+                      style: TextStyle(
+                        fontSize: 15,
                       ),
+                    ),
 
-                      //Text("${}")
-                      //Text(profile.lastVisitTime.toString())
-                    ],
-                  ),
+                    //Text("${}")
+                    //Text(profile.lastVisitTime.toString())
+                  ],
                 ),
-              ),
-            )
-        ],
+              )
+          ],
+        ),
       ),
     );
   }
