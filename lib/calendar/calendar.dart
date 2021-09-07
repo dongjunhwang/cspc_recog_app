0fa620287f8009c1d09ee73d5efe0c4654b430ca
huttzza <dobbie6174@gmail.com>
2021-09-08 01:22:00 +0900

    update calendar event UI

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -97,147 +97,175 @@ class _CalendarState extends State<Calendar> {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            /*Container(
-              height: height * 0.11,
-              color: themecolor[0],
-            ),*/
-            SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TableCalendar(
-                      locale: 'ko-KR',
-                      firstDay: DateTime(1960),
-                      focusedDay: selectedDay,
-                      lastDay: DateTime(2100),
-                      calendarFormat: format,
-                      onFormatChanged: (CalendarFormat _format) {
-                        setState(() {
-                          format = _format;
-                        });
-                      },
-                      onDaySelected: (DateTime selectDay, DateTime focusDay) {
-                        setState(() {
-                          selectedDay = selectDay;
-                          focusedDay = focusDay;
-                        });
-                      },
-                      eventLoader: _getEventsFromDay,
-                      /*Calendar Style*/
-                      calendarStyle: CalendarStyle(
-                          isTodayHighlighted: true,
-                          selectedDecoration: BoxDecoration(
-                            color: themecolor[0],
-                            /*borderRadius:
-                                  BorderRadius.all(Radius.circular(5.0))*/
-                          ),
-                          selectedTextStyle: TextStyle(color: Colors.white),
-                          todayDecoration: BoxDecoration(
-                            color: Colors.grey,
-                            /*borderRadius:
-                                  BorderRadius.all(Radius.circular(5.0))*/
-                          ),
-                          weekendTextStyle: TextStyle(color: Colors.grey)),
-                      selectedDayPredicate: (DateTime date) {
-                        return isSameDay(selectedDay, date);
-                      },
-                      /*Calendar Header Style*/
-                      headerStyle: HeaderStyle(
-                        /* Header Box */
-                        decoration: BoxDecoration(color: themecolor[0]),
-                        headerMargin: EdgeInsets.only(bottom: height * 0.02),
-                        headerPadding: EdgeInsets.only(
-                            right: width * 0.05,
-                            left: width * 0.05,
-                            top: height * 0.12,
-                            bottom: height * 0.02),
-                        /*title*/
-                        titleTextStyle:
-                            TextStyle(color: Colors.white, fontSize: 20),
-                        //titleCentered: true,
-                        /* Format Button */
-                        formatButtonVisible: true,
-                        formatButtonShowsNext: false,
-                        formatButtonDecoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        formatButtonTextStyle: TextStyle(color: Colors.white),
-                        /* Calendar arrow */
-                        leftChevronVisible: false,
-                        rightChevronVisible: false,
-                      ),
-                      daysOfWeekStyle: DaysOfWeekStyle(
-                          weekdayStyle: TextStyle(fontSize: 13),
-                          weekendStyle: TextStyle(fontSize: 13)),
-                      calendarBuilders: CalendarBuilders(markerBuilder: (
-                        context,
-                        date,
-                        events,
-                      ) {
-                        if (events.isNotEmpty) {
-                          return Positioned(
-                            bottom: 3,
-                            height: 5,
-                            child: Container(
-                              width: 30,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.rectangle,
-                                  color: Color(0xc0fa897b)),
-                            ),
-                          );
-                        }
-                        return Container();
-                      })),
-                ],
+            Stack(children: [
+              Container(
+                height: height * 0.18,
+                color: themecolor[0],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0, top: 8.0),
-              child: Text(
-                  DateFormat("yyyy.MM.dd EE", 'ko-KR').format(selectedDay),
-                  style: Theme.of(context).textTheme.headline6),
+              SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TableCalendar(
+                        locale: 'ko-KR',
+                        firstDay: DateTime(1960),
+                        focusedDay: selectedDay,
+                        lastDay: DateTime(2100),
+                        calendarFormat: format,
+                        onFormatChanged: (CalendarFormat _format) {
+                          setState(() {
+                            format = _format;
+                          });
+                        },
+                        onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                          setState(() {
+                            selectedDay = selectDay;
+                            focusedDay = focusDay;
+                          });
+                        },
+                        eventLoader: _getEventsFromDay,
+                        /*Calendar Style*/
+                        calendarStyle: CalendarStyle(
+                            isTodayHighlighted: true,
+                            selectedDecoration: BoxDecoration(
+                              color: themecolor[0],
+                              /*borderRadius:
+                                    BorderRadius.all(Radius.circular(5.0))*/
+                            ),
+                            selectedTextStyle: TextStyle(color: Colors.white),
+                            todayDecoration: BoxDecoration(
+                              color: Colors.black26,
+                              /*borderRadius:
+                                    BorderRadius.all(Radius.circular(5.0))*/
+                            ),
+                            weekendTextStyle: TextStyle(color: Colors.black45)),
+                        selectedDayPredicate: (DateTime date) {
+                          return isSameDay(selectedDay, date);
+                        },
+                        /*Calendar Header Style*/
+                        headerStyle: HeaderStyle(
+                          /* Header Box */
+                          decoration: BoxDecoration(color: themecolor[0]),
+                          headerMargin: EdgeInsets.only(bottom: height * 0.02),
+                          headerPadding: EdgeInsets.only(
+                              right: width * 0.05,
+                              left: width * 0.05,
+                              top: height * 0.12,
+                              bottom: height * 0.015),
+                          /*title*/
+                          titleTextStyle:
+                              TextStyle(color: Colors.white, fontSize: 20),
+                          //titleCentered: true,
+                          /* Format Button */
+                          formatButtonVisible: true,
+                          formatButtonShowsNext: false,
+                          formatButtonDecoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          formatButtonTextStyle: TextStyle(color: Colors.white),
+                          /* Calendar arrow */
+                          leftChevronVisible: false,
+                          rightChevronVisible: false,
+                        ),
+                        daysOfWeekStyle: DaysOfWeekStyle(
+                            weekdayStyle: TextStyle(fontSize: 13),
+                            weekendStyle: TextStyle(fontSize: 13)),
+                        calendarBuilders: CalendarBuilders(markerBuilder: (
+                          context,
+                          date,
+                          events,
+                        ) {
+                          if (events.isNotEmpty) {
+                            return Positioned(
+                              bottom: 3,
+                              height: 5,
+                              child: Container(
+                                width: 30,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.rectangle,
+                                    color: Color(0xc0fa897b)),
+                              ),
+                            );
+                          }
+                          return Container();
+                        })),
+                  ],
+                ),
+              ),
+            ]),
+            Container(
+              height: height * 0.02,
             ),
             Expanded(
-              child: MediaQuery.removePadding(
-                context: context,
-                removeTop: true,
-                child: ListView.separated(
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    itemCount: getEventsFromDay.length,
-                    shrinkWrap: true,
-                    itemBuilder: (BuildContext context, int index) {
-                      final event = getEventsFromDay[index];
-                      String startDateFormat = "MM.dd EE a hh:mm";
-                      String endDateFormat = "MM.dd EE a hh:mm";
-                      if (getOnlyDate(event.start_date)
-                          .isAtSameMomentAs(selectedDay)) {
-                        startDateFormat = "a hh:mm";
-                      }
-                      if (getOnlyDate(event.end_date)
-                          .isAtSameMomentAs(selectedDay)) {
-                        endDateFormat = "a hh:mm";
-                      }
-                      String startDateStr = DateFormat(startDateFormat, "ko-kr")
-                          .format(event.start_date);
-                      String endDateStr = DateFormat(endDateFormat, "ko-kr")
-                          .format(event.end_date);
-                      return ListTile(
-                          title: Text(
-                            event.title,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          subtitle: Text(startDateStr + ' - ' + endDateStr),
-                          onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => EventDetails(
-                                        event: event,
-                                      ))).then((value) => setState(() {
-                                _fetchEvent();
-                              })));
-                    }),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      blurRadius: 5,
+                      offset: Offset(0, -3)),
+                ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 15.0, top: 8.0, bottom: 3),
+                      child: Text(
+                          DateFormat("yyyy.MM.dd EE", 'ko-KR')
+                              .format(selectedDay),
+                          style: TextStyle(fontSize: width * 0.045)),
+                      //Theme.of(context).textTheme.headline6),
+                    ),
+                    Expanded(
+                      child: MediaQuery.removePadding(
+                        context: context,
+                        removeTop: true,
+                        child: ListView.separated(
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                            itemCount: getEventsFromDay.length,
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              final event = getEventsFromDay[index];
+                              String startDateFormat = "MM.dd EE a hh:mm";
+                              String endDateFormat = "MM.dd EE a hh:mm";
+                              if (getOnlyDate(event.start_date)
+                                  .isAtSameMomentAs(selectedDay)) {
+                                startDateFormat = "a hh:mm";
+                              }
+                              if (getOnlyDate(event.end_date)
+                                  .isAtSameMomentAs(selectedDay)) {
+                                endDateFormat = "a hh:mm";
+                              }
+                              String startDateStr =
+                                  DateFormat(startDateFormat, "ko-kr")
+                                      .format(event.start_date);
+                              String endDateStr =
+                                  DateFormat(endDateFormat, "ko-kr")
+                                      .format(event.end_date);
+                              return ListTile(
+                                  title: Text(
+                                    event.title,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  subtitle:
+                                      Text(startDateStr + ' - ' + endDateStr),
+                                  onTap: () => Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => EventDetails(
+                                                event: event,
+                                              ))).then((value) => setState(() {
+                                        _fetchEvent();
+                                      })));
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
